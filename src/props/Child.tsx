bd208a532ed9fb1075d5139a087755c86a8e85e5
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-01-08 22:53:44 +0530

    Props & Component Type

---
+++ src/props/Child.tsx
@@ -0,0 +1,15 @@
+interface ChildProps {
+  color: string
+}
+
+export const Child = ({ color }: ChildProps) => {
+  return <div>{color}</div>
+}
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>
+}
+
+ChildAsFC.defaultProps = {
+  color: 'blue',
+}
